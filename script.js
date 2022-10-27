7e8ab61449a37e39ec6d105375e6d56ab6a80d72
Rdest026 <rafaelsbo1@hotmail.com>
2022-10-27 20:41:41 -0300

    third commit

--- script.js
+++ script.js
@@ -2,8 +2,6 @@ function logar(event) {
   event.preventDefault();
 
   const data = {
-    // email: "eve.holt@reqres.in",
-    // password: "rafael123"
     email: this.document.getElementById("input-email").value,
     password: this.document.getElementById("input-password").value,
   };
@@ -23,6 +21,7 @@ function logar(event) {
         document.getElementById("modal-login").classList.remove("visible");
         document.getElementById("button-login").classList.remove("visible");
         document.getElementById("button-logout").classList.add("visible");
+        document.getElementById("content-API").classList.add("visible");
       }
     })
     .catch((error) => {
@@ -33,6 +32,7 @@ function logar(event) {
 if (localStorage.getItem("token") != null) {
   document.getElementById("button-login").classList.remove("visible");
   document.getElementById("button-logout").classList.add("visible");
+  document.getElementById("content-API").classList.add("visible");
 }
 
 function abreLogin() {
@@ -46,4 +46,39 @@ function fechaLogin() {
 function deslogar() {
   localStorage.removeItem("token");
   window.location.reload();
+}
+
+function pesquisar(event) {
+  event.preventDefault();
+
+  const name = this.document.getElementById("input-search").value;
+  const requestName =
+    "https://omgvamp-hearthstone-v1.p.rapidapi.com/cards/search/" + name;
+  const cardTemplate = document.querySelector("[card-template]");
+  const cardContainer = document.querySelector("[content-card]");
+  const data = {
+    method: "GET",
+    headers: {
+      "X-RapidAPI-Key": "184021a6acmsh35a297e24f4750bp131fd2jsn47732ad2c6a3",
+      "X-RapidAPI-Host": "omgvamp-hearthstone-v1.p.rapidapi.com",
+    },
+  };
+
+  cardContainer.innerHTML = "";
+
+  fetch(requestName, data)
+    .then((response) => response.json())
+    .then((response) => {
+      response.forEach((element) => {
+        if (element.img != null) {
+          const card = cardTemplate.content.cloneNode(true).children[0];
+          const cardImage = card.querySelector("[card-image]");
+          cardImage.src = element.img;
+          cardContainer.append(card);
+        }
+      });
+    })
+    .catch((err) => {
+      alert("Carta não encontrada.");
+    });
 }
